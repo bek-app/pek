a1404ac48bfd42c05324e1962c991dc00119502c
Zhussipov Meirambek <make202006@gmail.com>
2022-04-08 22:13:32 +0600

    add object-list

---
+++ src/app/components/object-list/object-form/object-form.component.ts
@@ -0,0 +1,190 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ObjectModel } from '@models/objects/object.model';
+import { DicKatoService } from '@services/dictionary/dic-kato.service';
+import { ObjectsService } from '@services/objects/objects.service';
+import { Subscription } from 'rxjs';
+
+const trackByIdentity = (index: number, item: any) => item;
+
+@Component({
+  selector: 'app-object-form',
+  templateUrl: './object-form.component.html',
+  styleUrls: ['./object-form.component.scss'],
+})
+export class ObjectFormComponent implements OnInit {
+  isActive = false;
+  form: FormGroup;
+  submitted?: boolean;
+  oblast: any[] = [];
+  region: any[] = [];
+  subRegion: any[] = [];
+  village: any[] = [];
+  oblastName!: string;
+  regionName!: string;
+  subRegionName!: string;
+  villageName!: string;
+  address!: string;
+  viewMode = false;
+
+  @Output() objectCreated = new EventEmitter<ObjectModel>();
+  @Output() objectUpdated = new EventEmitter<ObjectModel>();
+  subs!: Subscription;
+  constructor(
+    private fb: FormBuilder,
+    private dicKatoService: DicKatoService,
+    private objectsService: ObjectsService
+  ) {
+    this.form = this.fb.group({
+      nameObject: new FormControl('', Validators.required),
+      oblastId: new FormControl('', Validators.required),
+      regionId: new FormControl('', Validators.required),
+      subRegionId: new FormControl(),
+      villageId: new FormControl(),
+      address: new FormControl(),
+      inactive: new FormControl(true, Validators.required),
+    });
+  }
+
+  ngOnInit(): void {
+    this.dicKatoService.getDicKato(1).subscribe((data: any[]) => {
+      this.oblast = data;
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    const data = { externalUserId: 1, ...this.form.value };
+    !this.isActive
+      ? this.objectCreated.emit(data)
+      : this.objectUpdated.emit(data);
+  }
+
+  editForm(id: number) {
+    this.isActive = true;
+    this.objectsService.getObject(id).subscribe((data) => {
+      this.form.patchValue(data);
+    });
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  oblastChange(oblastId: number) {
+    if (oblastId) {
+      this.oblastName = this.oblast.find(
+        (obl) => obl.id === this.form.value.oblastId
+      )?.name;
+
+      if (this.oblastName) {
+        this.form.controls['address'].setValue(this.oblastName);
+        this.form.controls['regionId'].setValue(null);
+        this.subRegion = [];
+        this.village = [];
+      }
+      this.dicKatoService.getDicKato(oblastId).subscribe((region: any[]) => {
+        this.region = region;
+      });
+    }
+  }
+
+  regionChange(regionId: number) {
+    if (regionId) {
+      this.oblastName = this.oblast.find(
+        (obl) => obl.id === this.form.value.oblastId
+      )?.name;
+
+      this.regionName = this.region.find((reg) => reg.id === regionId)?.name;
+
+      if (this.oblastName && this.regionName) {
+        this.address = `${this.oblastName}, ${this.regionName}`;
+        this.form.controls['address'].setValue(this.address);
+        this.form.value.villageId = null;
+        this.form.value.subRegionId = null;
+        this.village = [];
+      }
+
+      this.dicKatoService.getDicKato(regionId).subscribe((subRegion: any[]) => {
+        this.subRegion = subRegion;
+      });
+    }
+  }
+
+  subRegionChange(subRegionId: number) {
+    if (subRegionId) {
+      this.subRegionName = this.subRegion.find(
+        (subReg) => subReg.id === subRegionId
+      )?.name;
+
+      if (!this.oblastName)
+        this.oblastName = this.oblast.find(
+          (obl) => obl.id === this.form.value.oblastId
+        )?.name;
+
+      if (!this.regionName)
+        this.regionName = this.region.find(
+          (reg) => reg.id === this.form.value.regionId
+        )?.name;
+
+      if (this.oblastName && this.regionName && this.subRegionName) {
+        this.address = `${this.oblastName}, ${this.regionName}, ${this.subRegionName} `;
+        this.form.controls['address'].setValue(this.address);
+      }
+      this.dicKatoService
+        .getDicKato(subRegionId)
+        .subscribe((village: any[]) => {
+          this.village = village;
+        });
+    }
+  }
+
+  villageChange(villageId: number) {
+    if (villageId) {
+      this.villageName = this.village.find(
+        (vill) => vill.id === villageId
+      )?.name;
+
+      if (!this.oblastName)
+        this.oblastName = this.oblast.find(
+          (obl) => obl.id === this.form.value.oblastId
+        )?.name;
+
+      if (!this.regionName)
+        this.regionName = this.region.find(
+          (reg) => reg.id === this.form.value.regionId
+        )?.name;
+
+      if (!this.subRegionName)
+        this.subRegionName = this.subRegion.find(
+          (subReg) => subReg.id === this.form.value.subRegionId
+        )?.name;
+
+      if (
+        this.oblastName &&
+        this.regionName &&
+        this.subRegionName &&
+        this.villageName
+      ) {
+        this.address = `${this.oblastName}, ${this.regionName}, ${this.subRegionName}, ${this.villageName}`;
+        this.form.controls['address'].setValue(this.address);
+      }
+    }
+  }
+}
--- src/app/components/object-list/object-list.component.ts
+++ src/app/components/object-list/object-list.component.ts
@@ -1,15 +1,224 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  AngularGridInstance,
+  Column,
+  FieldType,
+  Formatters,
+  GridOption,
+  OnEventArgs,
+} from 'angular-slickgrid';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ObjectModel } from '@models/objects/object.model';
+import { ObjectsService } from '@services/objects/objects.service';
+import { ObjectFormComponent } from './object-form/object-form.component';
 @Component({
   selector: 'app-object-list',
   templateUrl: './object-list.component.html',
-  styleUrls: ['./object-list.component.scss']
+  styleUrls: ['./object-list.component.scss'],
 })
 export class ObjectListComponent implements OnInit {
+  angularGrid!: AngularGridInstance;
+  columnDefinitions!: Column[];
+  gridOptions!: GridOption;
+  dataset: ObjectModel[] = [];
+  gridObj: any;
+  dataViewObj: any;
+  objectId!: number;
+  objectFormRef: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  angularGridReady(angularGrid: AngularGridInstance) {
+    this.angularGrid = angularGrid;
+    this.gridObj = angularGrid.slickGrid;
+    this.dataViewObj = angularGrid.dataView;
   }
 
+  constructor(
+    private objectsService: ObjectsService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private dialog: MatDialog
+  ) {}
+
+  ngOnInit(): void {
+    this.refreshList();
+    this.defineGrid();
+  }
+
+  refreshList() {
+    this.objectsService.getObjects().subscribe((data) => {
+      this.dataset = data;
+    });
+  }
+
+  openObjectDialog() {
+    this.objectFormRef = this.dialog.open(ObjectFormComponent);
+    this.onObjectAdded();
+    this.onObjectUpdated();
+  }
+
+  onObjectAdded() {
+    this.objectFormRef.componentInstance.objectCreated.subscribe({
+      next: (data: ObjectModel) => {
+        this.objectsService.addObject(data).subscribe({
+          next: () => this.objectFormRef.close(),
+        });
+      },
+    });
+  }
+
+  onObjectUpdated() {
+    this.objectFormRef.componentInstance.objectUpdated.subscribe({
+      next: (data: ObjectModel) => {
+        this.objectsService
+          .updateObject({ id: this.objectId, ...data })
+          .subscribe({
+            next: () => {
+              this.objectFormRef.close(), this.refreshList();
+            },
+          });
+      },
+    });
+  }
+
+  onCellClicked(e: any, args: any) {
+    const item = this.gridObj.getDataItem(args.row);
+    this.objectId = item.id;
+  }
+
+  defineGrid() {
+    this.columnDefinitions = [
+      {
+        id: 'nameObject',
+        name: 'Наименование Обьекта',
+        field: 'nameObject',
+        filterable: true,
+        sortable: true,
+      },
+      {
+        id: 'oblastName',
+        name: 'Область',
+        field: 'oblastName',
+        filterable: true,
+        sortable: true,
+      },
+      {
+        id: 'regionName',
+        name: 'Город',
+        field: 'regionName',
+        filterable: true,
+        sortable: true,
+      },
+
+      {
+        id: 'subRegionName',
+        name: 'Сельский округ',
+        field: 'subRegionName',
+        filterable: true,
+        sortable: true,
+      },
+      {
+        id: 'villageName',
+        name: 'Село',
+        field: 'villageName',
+        filterable: true,
+        sortable: true,
+      },
+      {
+        id: 'address',
+        name: 'Адресс',
+        field: 'address',
+        filterable: true,
+        sortable: true,
+      },
+      {
+        id: 'inactive',
+        name: 'Ликвидирован',
+        field: 'inactive',
+        filterable: true,
+        type: FieldType.boolean,
+        sortable: true,
+        exportCustomFormatter: Formatters.complexObject,
+        formatter: Formatters.multiple,
+        params: {
+          formatters: [Formatters.complexObject, Formatters.checkmark],
+        },
+      },
+      {
+        id: 'view',
+        field: 'id',
+        excludeFromColumnPicker: true,
+        excludeFromGridMenu: true,
+        excludeFromHeaderMenu: true,
+        formatter: () => `<i class="fa fa-eye pointer"></i>`,
+        minWidth: 30,
+        maxWidth: 30,
+        onCellClick: (e: Event, args: OnEventArgs) => {
+          this.openObjectDialog();
+          this.objectFormRef.componentInstance.editForm(this.objectId);
+          this.objectFormRef.componentInstance.form.disable();
+          this.objectFormRef.componentInstance.viewMode = true;
+        },
+      },
+      {
+        id: 'action',
+        field: 'action',
+        minWidth: 30,
+        maxWidth: 30,
+        excludeFromColumnPicker: true,
+        excludeFromGridMenu: true,
+        excludeFromHeaderMenu: true,
+        formatter: () => `<i class="fa fa-cog" style="cursor:pointer;"></i>`,
+        onCellClick: (e: Event, args: OnEventArgs) => {
+          const id = args.dataContext.id;
+          this.router.navigate(['../objects', id], {
+            relativeTo: this.route,
+          });
+        },
+      },
+      {
+        id: 'edit',
+        field: 'id',
+        excludeFromColumnPicker: true,
+        excludeFromGridMenu: true,
+        excludeFromHeaderMenu: true,
+        formatter: Formatters.editIcon,
+        minWidth: 30,
+        maxWidth: 30,
+        onCellClick: (e: Event, args: OnEventArgs) => {
+          const id = args.dataContext.id;
+          this.openObjectDialog();
+          this.objectFormRef.componentInstance.editForm(id);
+        },
+      },
+      {
+        id: 'delete',
+        field: 'delete',
+        excludeFromColumnPicker: true,
+        excludeFromGridMenu: true,
+        excludeFromHeaderMenu: true,
+        formatter: Formatters.deleteIcon,
+        minWidth: 30,
+        maxWidth: 30,
+        onCellClick: (e: Event, args: OnEventArgs) => {
+          const id = args.dataContext.id;
+          if (confirm('Are you sure?')) {
+            this.objectsService.deleteObject(id).subscribe(() => {
+              this.refreshList();
+            });
+          }
+        },
+      },
+    ];
+
+    this.gridOptions = {
+      enableFiltering: true,
+      enableSorting: true,
+      enablePagination: true,
+      pagination: {
+        pageSizes: [5, 10, 20, 25, 50],
+        pageSize: 20,
+      },
+    };
+  }
 }
